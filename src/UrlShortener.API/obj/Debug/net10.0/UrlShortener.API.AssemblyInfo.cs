09aaab6d35ff32c98285e376a66ffc8c3360ede5
divyanjali2 <navodyadivyanjali2@gmail.com>
2026-04-09 17:17:23 +0530

    updates added

--- src/UrlShortener.API/obj/Debug/net10.0/UrlShortener.API.AssemblyInfo.cs
+++ src/UrlShortener.API/obj/Debug/net10.0/UrlShortener.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrlShortener.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2caac7e569ea65dffabc05a4d8091d486a833910")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+205e7f48ca4a26a65086698bd63cd8914a5ce8c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrlShortener.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrlShortener.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
